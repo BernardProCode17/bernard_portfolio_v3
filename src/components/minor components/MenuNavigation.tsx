1cb8cda662bbff2afe1dead776b26f78007ed529
BernardProCode17 <bclarke42@my.bcit.ca>
2024-08-19 15:01:36 -0700

    completed header navigation menu

--- src/components/minor components/MenuNavigation.tsx
+++ src/components/minor components/MenuNavigation.tsx
@@ -9,39 +9,41 @@ export default function MenuNavigation() {
     const [isOpen, setIsOpen] = useState(false)
     isOpenExport = setIsOpen;
 
-    function closeButton(){
+    function closeButton() {
         setIsOpen(false)
     }
 
     return (
-        <nav  className={isOpen ? 'header_nav-active' : 'header_nav'}>
-            <ul className='header_nav-ul'>
+        // <div className='header_nav-container'>
+            < nav className={isOpen ? 'header_nav-active' : 'header_nav'} >
+                <ul className='header_nav-ul'>
 
-                <li className='header_nav-li'>
-                    <Link href='/' className='header_nav-link'>Home</Link>
-                </li>
+                    <li className='header_nav-li'>
+                        <Link href='/' className='header_nav-link header_nav_link-active'>Home</Link>
+                    </li>
 
-                <li className='header_nav-li'>
-                    <Link href='/project' className='header_nav-link'>Projects</Link>
-                </li>
+                    <li className='header_nav-li'>
+                        <Link href='/project' className='header_nav-link'>Projects</Link>
+                    </li>
 
-                <li className='header_nav-li'>
-                    <Link href='/skills' className='header_nav-link'>Skills</Link>
-                </li>
+                    <li className='header_nav-li'>
+                        <Link href='/skills' className='header_nav-link'>Skills</Link>
+                    </li>
 
-                <li className='header_nav-li'>
-                    <Link href='/about' className='header_nav-link'>About</Link>
-                </li>
+                    <li className='header_nav-li'>
+                        <Link href='/about' className='header_nav-link'>About</Link>
+                    </li>
 
-                <li className='header_nav-li'>
-                    <Link href='/contact' className='header_nav-link'>Contact</Link>
-                </li>
+                    <li className='header_nav-li'>
+                        <Link href='/contact' className='header_nav-link'>Contact</Link>
+                    </li>
 
-                <li className='header_nav-li'>
-                    <Link href='/votd' className='header_nav-link'>Verse Of The Day</Link>
-                </li>
-            </ul>
-            <Button content="Close" clickFunction={closeButton} />
-        </nav>
+                    <li className='header_nav-li'>
+                        <button className='header_nav_link-votd'>Verse Of The Day</button>
+                    </li>
+                </ul>
+                <Button content="Close" clickFunction={closeButton} />
+            </nav>
+        // </div>
     )
 }
