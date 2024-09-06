e48c1357fe669f479bf3148e340aa19841e84015
Bernard Clarke <bernardclarke@bernardwebpro.com>
2024-09-06 13:53:25 -0700

    Completed Home page banner and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,23 @@
-import Button from "@/components/minor components/Button";
 import HeadingSection from "@/components/minor components/HeadingSection";
 import { JSLottie } from "@/components/minor components/lottie";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <HeadingSection cssClass="home_header">
         <div className="home_header-intro">
-          <h1>Bernard Clarke</h1>
-          <h2>Front-End Javascript Web Developer</h2>
-          <p>Front-end Developer specializing in JavaScript base development with focus on React and Next.js</p>
+          <h1 className="header_intro-title">Bernard Clarke</h1>
+          <h2 className="header_intro-subtitle">Front-End Javascript Web Developer</h2>
+          <p className="header_intro-content">Front-end Developer specializing in JavaScript base development with focus on React and Next.js</p>
         </div>
 
         {/* Jesus */}
         <JSLottie />
 
-        <div className="home_header-buttons">
-          <Button content='Projects' />
-          <Button content='Skills' />
-
+        <div className="home_header-links">
+          <Link href='projects' className="home_header_links-projects">Projects</Link>
+          <Link href='skills' className="home_header_links-skills">Skills</Link>
         </div>
 
       </HeadingSection>
