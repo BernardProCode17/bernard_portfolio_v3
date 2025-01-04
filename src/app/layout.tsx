86245d0cf37e3ca0f97bc885e2f9d15895262106
BernardProCode17 <bclarke42@my.bcit.ca>
2025-01-03 19:17:41 -0800

    Add @vercel/speed-insights package and integrate in layout
    Integrated the SpeedInsights React component in the app's layout for performance insights. Updated `package.json` and `package-lock.json` to include the new dependency. Adjusted formatting for consistency in the `RootLayout` component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
-import { Metadata } from "next/types";
+import {Metadata} from "next/types";
 import "../styles/globals.css";
 import Header from "@/components/header_footer/Header";
 import VOTD from "@/components/VOTD/VOTD";
 import Footer from "@/components/header_footer/Footer";
-import { Suspense } from "react";
+import {Suspense} from "react";
 import Loading from "./loading";
+import {SpeedInsights} from "@vercel/speed-insights/react"
 
 export const metadata: Metadata = {
-  title: "Bernard Clarke | Front-end Javascript React developer",
-  description: "Bernard Clarke is a front-end Javascript React developer based in vancouver, BC.I have a passion for creating beautiful, responsive, and accessible web applications and website. I'm currently looking for an opportunity to work with react, typescript, and next.js to create amazing application and develop my skills to become a full stack developer.",
+    title: "Bernard Clarke | Front-end Javascript React developer",
+    description: "Bernard Clarke is a front-end Javascript React developer based in vancouver, BC.I have a passion for creating beautiful, responsive, and accessible web applications and website. I'm currently looking for an opportunity to work with react, typescript, and next.js to create amazing application and develop my skills to become a full stack developer.",
 };
 
 
-export default function RootLayout({ children, }:{ children: React.ReactNode; }) {
+export default function RootLayout({children,}: { children: React.ReactNode; }) {
 
-  return (
-    <html lang="en">
+    return (
+        <html lang="en">
 
-      <body className="body">
-        <Header />
-        <VOTD />
+        <body className="body">
+        <Header/>
+        <VOTD/>
         <Suspense fallback={<Loading/>}>
-          {children}
+            {children}
+            <SpeedInsights/>
         </Suspense>
-        <Footer />
-      </body>
+        <Footer/>
+        </body>
 
-    </html>
-  );
+        </html>
+    );
 }
