62918bcf9441b1f052e141d66564442f43c621d3
Bernard Clarke <bernardanthonyclarke1292@gmail.com>
2025-10-05 11:39:44 -0700

    Updated skills lottie - removed isClient and added next/dynamic

--- src/components/minor components/lotties/lottie.tsx
+++ src/components/minor components/lotties/lottie.tsx
@@ -1,6 +1,8 @@
 'use client'
-import React, {useEffect, useState} from 'react';
-import Lottie from 'react-lottie'
+// import React, {useEffect, useState} from 'react';
+// import Lottie from 'react-lottie'
+import React from 'react';
+import dynamic from 'next/dynamic'
 import projectsLottie from '../../../assets/project_folder_lottie.json'
 import skillLottie from '../../../assets/skills_graph_lottie.json'
 import developmentSkills from '../../../assets/development_skills_lottie.json'
@@ -10,16 +12,22 @@ import developmentTools from '../../../assets/developement_tools_skills_lottie.j
 // type LottieProps = { display: string }
 //const clientDocument = typeof window !== 'undefined'
 
+const Lottie = dynamic(() => import('react-lottie'), {
+        ssr: false,
+        loading: () => <p>Loading...</p>,
+    }
+);
+
 
 export function ProjectsLottie() {
 
-    const [isClient, setIsClient] = useState(false);
+    // const [isClient, setIsClient] = useState(false);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsClient(true);
-        }
-    }, []);
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         setIsClient(true);
+    //     }
+    // }, []);
 
     const options = {
         loop: true,
@@ -30,20 +38,21 @@ export function ProjectsLottie() {
 
     return (
         <div className={`home_projects-lottie`}>
-            {isClient && <Lottie options={options} width={'100%'} isClickToPauseDisabled={true}/>}
+            <Lottie options={options} width={'100%'} isClickToPauseDisabled={true}/>
         </div>
     )
 }
 
 export function SkillsLottie() {
 
-    const [isClient, setIsClient] = useState(false);
+    // const [isClient, setIsClient] = useState(false);
+    //
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         setIsClient(true);
+    //     }
+    // }, []);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsClient(true);
-        }
-    }, []);
     const options = {
         loop: true,
         autoplay: true,
@@ -53,20 +62,20 @@ export function SkillsLottie() {
 
     return (
         <div className="home_skills-lottie">
-            {isClient && <Lottie options={options} width={200} isClickToPauseDisabled={true}/>}
+            <Lottie options={options} width={200} isClickToPauseDisabled={true}/>
         </div>
     )
 }
 
 export function DevelopmentSkillsLottie() {
 
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsClient(true);
-        }
-    }, []);
+    // const [isClient, setIsClient] = useState(false);
+    //
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         setIsClient(true);
+    //     }
+    // }, []);
 
     const options = {
         loop: true,
@@ -77,19 +86,20 @@ export function DevelopmentSkillsLottie() {
 
     return (
         <div className="home_skills-lottie home_skills_lottie-display">
-            {isClient && <Lottie options={options} width={200} isClickToPauseDisabled={true}/>}
+            <Lottie options={options} width={200} isClickToPauseDisabled={true}/>
         </div>
     )
 }
 
 export function CommunicationSkillsLottie() {
-    const [isClient, setIsClient] = useState(false);
+    // const [isClient, setIsClient] = useState(false);
+    //
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         setIsClient(true);
+    //     }
+    // }, []);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsClient(true);
-        }
-    }, []);
     const options = {
         loop: true,
         autoplay: true,
@@ -99,20 +109,20 @@ export function CommunicationSkillsLottie() {
 
     return (
         <div className="home_skills-lottie home_skills_lottie-display">
-            {isClient && <Lottie options={options} width={200} isClickToPauseDisabled={true}/>}
+            <Lottie options={options} width={200} isClickToPauseDisabled={true}/>
         </div>
     )
 }
 
 export function DevelopmentToolsLottie() {
 
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsClient(true);
-        }
-    }, []);
+    // const [isClient, setIsClient] = useState(false);
+    //
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         setIsClient(true);
+    //     }
+    // }, []);
 
     const options = {
         loop: true,
@@ -123,20 +133,20 @@ export function DevelopmentToolsLottie() {
 
     return (
         <div className="home_skills-lottie home_skills_lottie-display">
-            {isClient && <Lottie options={options} width={200}/>}
+            <Lottie options={options} width={200}/>
         </div>
     )
 }
 
 export function SkillImageLottie(lottieFile: object) {
 
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            setIsClient(true);
-        }
-    }, []);
+    // const [isClient, setIsClient] = useState(false);
+    //
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+    //         setIsClient(true);
+    //     }
+    // }, []);
 
     const options = {
         loop: true,
@@ -147,7 +157,7 @@ export function SkillImageLottie(lottieFile: object) {
 
     return (
         <div className="skill_image">
-            {isClient && <Lottie options={options} width={200}/>}
+            <Lottie options={options} width={200}/>
         </div>
     )
 }
